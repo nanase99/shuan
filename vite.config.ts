9dbc1b9947d130d16ab87b82376f83fdf390ca0a
nanase <sgmdq601@gmail.com>
2024-08-25 23:03:04 +0900

    add: remix-flat-routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { flatRoutes } from "remix-flat-routes";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     remix({
+      ignoredRouteFiles: ["**/*"],
+      routes: async (defineRoutes) => flatRoutes("routes", defineRoutes),
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
