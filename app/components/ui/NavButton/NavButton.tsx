3eded7b949c91e6d4311e33c666d4e18a944eaa4
nanase <sgmdq601@gmail.com>
2024-08-26 21:20:26 +0900

    feat: NavButtonを作成

---
+++ app/components/ui/NavButton/NavButton.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils";
+import { NavLink } from "@remix-run/react";
+import { Button } from "../Button";
+
+export type NavButtonProps = {
+  href: string;
+  label: string;
+  isActive?: boolean;
+};
+
+export function NavButton({ href, label, isActive }: NavButtonProps) {
+  return (
+    <Button
+      variant="outline"
+      size="sm"
+      asChild
+      className={cn(
+        "text-white transition border-none rounded hover:bg-white/20 hover:text-white focus:bg-white/30",
+        isActive ? "bg-white/10" : "bg-transparent",
+      )}
+    >
+      <NavLink to={href}>{label}</NavLink>
+    </Button>
+  );
+}
--- app/components/ui/NavButton/index.ts
+++ app/components/ui/NavButton/index.ts
@@ -1,5 +1,2 @@
-export { Button } from "./Button";
-export type { ButtonProps } from "./Button";
-
 export { NavButton } from "./NavButton";
 export type { NavButtonProps } from "./NavButton";
