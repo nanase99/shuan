45516bdc864910a99b36aaabe974b4d9b7c2594b
nanase <sgmdq601@gmail.com>
2024-09-10 21:29:42 +0900

    tmp: 教科機能に関する内容を一時的にコミット

--- app/routes/_commonLayout+/subjects/index.tsx
+++ app/routes/_commonLayout+/subjects/index.tsx
@@ -1,31 +1,20 @@
-import { SubjectCard } from "@/components/features/subjects";
-import { apiClient } from "@/libs";
-import { getAuth } from "@clerk/remix/ssr.server";
-import { type LoaderFunctionArgs, json, redirect } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { useEffect } from "react";
+import { SubjectCard } from "@/components/subjects";
+import { getSubjectUseCase } from "@/useCases";
+import { json } from "@remix-run/node";
 
-// export const loader = async (args: LoaderFunctionArgs) => {
-//   // const res = await apiClient.api.subjects.$get();
-//   // if (!res.ok) {
-//   //   throw new Response("Not Found", { status: 404 });
-//   // }
-//   // return json({ subjects: await res.json() });
-// };
+const { useGetSubjects, executePrefetch } = getSubjectUseCase;
+
+export const loader = async () => {
+  const res = await executePrefetch();
+  return json(res);
+};
 
 export default function Index() {
-  // const { subjects } = useLoaderData<typeof loader>();
-  useEffect(() => {
-    const fetcher = async () => {
-      const res = await fetch("http://localhost:5173/api/subjects");
-      if (res.ok) console.log(await res.json());
-    };
-    fetcher();
-  }, []);
-  // console.log(subjects);
+  const { data, isPending } = useGetSubjects();
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+      {/* {data?.subjects?.map((subject) => subject.id)} */}
       {/* {subjects.map((subject: any) => (
         <SubjectCard key={subject.id} subject={subject} />
       ))} */}
