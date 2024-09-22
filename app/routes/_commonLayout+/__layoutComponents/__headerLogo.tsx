dc7fc1c5873a148cdd0be9ff07cc021a7f295d0e
nanase <sgmdq601@gmail.com>
2024-09-22 23:56:03 +0900

    add: logoを差し替え

--- app/routes/_commonLayout+/__layoutComponents/__headerLogo.tsx
+++ app/routes/_commonLayout+/__layoutComponents/__headerLogo.tsx
@@ -3,8 +3,9 @@ import { Link } from "@/features/common/components/ui";
 export function HeaderLogo() {
   return (
     <Link href="/">
-      <div className="hidden lg:flex lg:items-center">
-        <p className="font-semibold text-white text-2xl ml-2">Logo</p>
+      <div className="hidden lg:flex lg:items-center gap-2">
+        <p className="font-semibold text-white text-2xl ml-2">週案アプリ</p>
+        <img src="/assets/logo.svg" alt="ロゴ" />
       </div>
     </Link>
   );
