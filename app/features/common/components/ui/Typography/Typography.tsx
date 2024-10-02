0411e3b690454ab7c2fb8401a60cec7fe38ab6b6
nanase <sgmdq601@gmail.com>
2024-10-02 17:24:28 +0900

    feat: 教科画面を仮作成

--- app/features/common/components/ui/Typography/Typography.tsx
+++ app/features/common/components/ui/Typography/Typography.tsx
@@ -9,7 +9,7 @@ export function Typography({ children, className }: TypographyProps) {
   return (
     <p
       className={cn(
-        "flex h-10 w-full bg-transparent px-3 py-2 text-2xl font-semibold items-center",
+        "flex h-10 w-full bg-transparent py-2 text-2xl font-semibold items-center",
         className,
       )}
     >
--- app/routes/_commonLayout+/subjects/index.tsx
+++ app/routes/_commonLayout+/subjects/index.tsx
@@ -1,7 +1,9 @@
 import { json } from "@remix-run/cloudflare";
 
-import { SubjectCard } from "@/features/subject/components";
+import { AddButton, Form } from "@/features/common/components/ui";
 import { fetchGetSubjects } from "@/features/subject/fetch/fetchGetSubjects";
+import { SubjectCard } from "./__SubjectCard";
+import { useSubjectsForm } from "./__hooks/useSubjectsForm";
 
 const { useGetSubjects, prefetchGetSubjects } = fetchGetSubjects();
 
@@ -12,12 +14,25 @@ export const loader = async () => {
 
 export default function Index() {
   const { data } = useGetSubjects();
+  const { form, subjectFields, handleSave, handleAddSubject } =
+    useSubjectsForm(data);
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-      {data?.map((subject) => {
-        return <SubjectCard key={subject.id} subject={subject} />;
-      })}
-    </div>
+    <Form {...form}>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        {subjectFields.map((field, index) => (
+          <SubjectCard
+            key={field.id}
+            index={index}
+            subject={field}
+            onSave={handleSave}
+            isView
+          />
+        ))}
+        <div className="grid place-items-center">
+          <AddButton type="button" onClick={handleAddSubject} />
+        </div>
+      </div>
+    </Form>
   );
 }
