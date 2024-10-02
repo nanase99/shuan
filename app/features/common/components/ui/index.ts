7723551436ff978bdf4d993d748f76c038784404
nanase <sgmdq601@gmail.com>
2024-10-02 16:27:31 +0900

    add: Formコンポーネントを作成

--- app/features/common/components/ui/index.ts
+++ app/features/common/components/ui/index.ts
@@ -1,16 +1,10 @@
-export { AddButton, type AddButtonProps } from "./AddButton";
-export { Button, type ButtonProps } from "./Button";
-export {
-  Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardDescription,
-  CardContent,
-} from "./Card";
-export { Input, type InputProps } from "./Input";
-export { Label } from "./Label";
-export { Link, type LinkProps } from "./Link";
-export { NavButton, type NavButtonProps } from "./NavButton";
-export { Typography, type TypographyProps } from "./Typography";
-export { UserIcon } from "./UserIcon";
+export * from "./AddButton";
+export * from "./Button";
+export * from "./Card";
+export * from "./Form";
+export * from "./Input";
+export * from "./Label";
+export * from "./Link";
+export * from "./NavButton";
+export * from "./Typography";
+export * from "./UserIcon";
