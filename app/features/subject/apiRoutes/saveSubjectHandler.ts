dbc17ff402c2bcee3bb97640870a3a58aaad3f0e
nanase <sgmdq601@gmail.com>
2024-11-05 09:00:40 +0900

    add: saveSubject Apiにバリデーションを追加

--- app/features/subject/apiRoutes/saveSubjectHandler.ts
+++ app/features/subject/apiRoutes/saveSubjectHandler.ts
@@ -1,13 +1,40 @@
+import { RowState } from "@/features/common/enums";
 import type { ServerEnv } from "@/server/serverUtil";
+import { vValidator } from "@hono/valibot-validator";
 import { createFactory } from "hono/factory";
+import * as v from "valibot";
 
 const factory = createFactory<ServerEnv>();
-
-export const saveSubjectHandler = factory.createHandlers(async (c) => {
-  // TODO: バリデーションを追加する
-  const values = await c.req.json();
-
-  const data = await c.var.saveSubjectUseCase.execute(values);
-
-  return c.json({ data });
+const schema = v.object({
+  id: v.string(),
+  subjectName: v.pipe(
+    v.string(),
+    v.nonEmpty("科目名を入力してください"),
+    v.maxLength(10, "科目名は10文字以下で入力してください"),
+  ),
+  classHours: v.number(),
+  progress: v.number(),
+  tag: v.string(),
+  courses: v.array(
+    v.object({
+      id: v.string(),
+      subjectId: v.string(),
+      courseName: v.pipe(v.string(), v.nonEmpty(), v.maxLength(10)),
+      classHours: v.number(),
+      progress: v.number(),
+      tag: v.string(),
+      rowState: v.enum(RowState),
+    }),
+  ),
 });
+
+export const saveSubjectHandler = factory.createHandlers(
+  vValidator("json", schema),
+  async (c) => {
+    const values = c.req.valid("json");
+
+    const data = await c.var.saveSubjectUseCase.execute(values);
+
+    return c.json({ data });
+  },
+);
--- app/features/subject/fetch/fetchPostSubject.ts
+++ app/features/subject/fetch/fetchPostSubject.ts
@@ -1,12 +1,19 @@
 import { getApiClient } from "@/features/common/libs";
 import { useMutation } from "@tanstack/react-query";
+import type { InferRequestType, InferResponseType } from "hono";
+
+const client = getApiClient();
+type ResponseType = InferResponseType<(typeof client.api.subjects)["$post"]>;
+type RequestType = InferRequestType<
+  (typeof client.api.subjects)["$post"]
+>["json"];
 
 export function fetchSaveSubject() {
   const useSaveSubject = () => {
-    const mutation = useMutation({
+    const mutation = useMutation<ResponseType, Error, RequestType>({
       mutationFn: async (json) => {
-        const response = getApiClient().api.subjects.$post({ json });
-        return (await response).json();
+        const response = await client.api.subjects.$post({ json });
+        return await response.json();
       },
     });
     return mutation;
